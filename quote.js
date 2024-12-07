cc4f08313a38c5f6817efc63ce8c8fc1dca95bf8
Chloé Cilia <chloe.cilia68@gmail.com>
2024-12-07 21:43:57 +0100

    exo5

---
+++ quote.js
@@ -0,0 +1,7 @@
+var quotes = {
+    "Serges Karamazov" : "Barrez vous **** de mimes.",
+    "Georges Aibitol" : "Le train de tes injures roule sur le rail de mon indifférence.",
+    "Hubert Bonisseur de la Bath" : "Ca ne prenait pas beauoup plus de temps.",
+    "Moulinier" : "L'habit ne fait pas le moine, mais il fait l'agent... Même non titularisé.",
+    "Orson Welles" : "C'est du vol et du plagiat. J'aime pas trop les voleurs..."
+}
